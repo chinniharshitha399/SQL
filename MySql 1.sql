aa88b1f7919465ed78862a1e04b40340976c7bce
harshitha <chinniharshitha399@gmail.com>
2026-01-23 00:39:26 -0500

    Set up school_db with Student_table and students
    Created a new database and multiple tables, added data, and modified table structures.

--- MySql 1.sql
+++ MySql 1.sql
@@ -1,8 +1,43 @@
-CREATE TABLE CUSTOMERS(
- ID INT NOT NULL,
- NAME VARCHAR (20) NOT NULL,
- AGE INT NOT NULL,
- ADDRESS CHAR (25) ,
- SALARY DECIMAL (18, 2),
- PRIMARY KEY (ID)
+--create database
+CREATE DATABASE school_db;
+USE school_db;
+----CREATE TABLE
+CREATE TABLE Student_table (
+    StudentID INT,
+    Name VARCHAR(50),
+    Age INT
 );
+----insert
+INSERT INTO Student_table (StudentID, Name, Age)
+VALUES
+(1, 'Alice', 20),
+(2, 'Bob', 22),
+(3, 'Charlie', 21);
+----select
+--SELECT * FROM Students; --for selecting all columns
+SELECT Name, Age FROM Student_table ;
+----alter
+SELECT * FROM Student_table;
+ALTER TABLE Student_table
+ADD Email VARCHAR(100);--(adding a column)
+--
+SELECT * FROM Student_table;
+ALTER TABLE Student_table
+RENAME COLUMN Email TO email_id;
+---
+ALTER TABLE Student_table
+MODIFY Name VARCHAR(100);--Modify the column size
+---
+DROP TABLE IF EXISTS Person;---drop means delete
+CREATE TABLE students(
+ ID INT NOT NULL UNIQUE,
+ LastName VARCHAR(55) NOT NULL,
+ FirstName VARCHAR(55) NOT NULL,
+ Age INT
+ );
+
+
+
+
+
+
